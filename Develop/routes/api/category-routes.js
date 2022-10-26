0b36d5b8a745c1a70faa9bd25c9c11b5fde3b117
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-26 15:44:28 -0500

    began category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,17 +1,47 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
+
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  // Find all categories & their associated products
+  try {
+    const categoryData =  Category.findAll(  // AWAIT   wont let me use await
+      {
+      include: [
+        {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+  
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // Find one category by its `id` value & associated Products
+  try {
+    const categoryData =  Category.findByPk(req.params.id, {   // await ?
+      // JOIN with Product, using the Trip through table
+      include: [{ model: Tag, through: ProductTag, as: 'product_id' }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
