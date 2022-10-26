a2ff1a1f8bdec5a9bc47fecc3977a69d8b91af4b
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-26 14:22:18 -0500

    setting up columns in Category.js and Product.js models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,15 +6,26 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }, 
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
   },
+},
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
+  },
+
 );
 
 module.exports = Category;
